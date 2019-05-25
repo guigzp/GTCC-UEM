7d948294ac10823f4b4b4bec02a8f11f5e88bffc
Viviane Shiraishi <vivianeshiraishi@gmail.com>
2019-05-25 18:01:05 -0300

    feat: Agendamento de defesa

---
+++ src/main/java/br/com/gtcc/model/Agendamento.java
@@ -0,0 +1,126 @@
+package br.com.gtcc.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import br.com.gtcc.util.AdapterLocalDate;
+
+/**
+ * 
+ * @author Grupo 03 - Ana Cláudia, Ana Paula, Rafael de Souza, Viviane Shiraishi
+ *
+ */
+
+@Entity
+public class Agendamento {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    @OneToOne
+	private FichaIdentificacao fichaIdentificacao;
+
+    @Convert(converter = AdapterLocalDate.class)
+	@DateTimeFormat(pattern="dd/MM/yyyy")
+	@NotNull(message = "Data de agendamento é uma informação obrigatória")
+	private LocalDate dataDefesa;
+
+    @Column(nullable = false)
+	@NotBlank(message = "Local é uma informação obrigatória")
+	private String local;
+
+    @Column(nullable = false, columnDefinition = "int(1) default 1")
+	private int ativo;
+    
+    
+
+	public Agendamento(Long id, FichaIdentificacao fichaIdentificacao,
+			@NotNull(message = "Data de agendamento é uma informação obrigatória") LocalDate dataDefesa,
+			@NotBlank(message = "Local é uma informação obrigatória") String local, int ativo) {
+		super();
+		this.id = id;
+		this.fichaIdentificacao = fichaIdentificacao;
+		this.dataDefesa = dataDefesa;
+		this.local = local;
+		this.ativo = ativo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public FichaIdentificacao getFichaIdentificacao() {
+		return fichaIdentificacao;
+	}
+
+	public void setFichaIdentificacao(FichaIdentificacao fichaIdentificacao) {
+		this.fichaIdentificacao = fichaIdentificacao;
+	}
+
+	public LocalDate getDataDefesa() {
+		return dataDefesa;
+	}
+
+	public void setDataDefesa(LocalDate dataDefesa) {
+		this.dataDefesa = dataDefesa;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public int getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(int ativo) {
+		this.ativo = ativo;
+	}
+    
+    
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agendamento other = (Agendamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+
+}
