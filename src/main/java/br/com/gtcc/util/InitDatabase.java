e9bfb56bc646fd8176b957025a25ba01ae7f7c05
Alisson Lopes <alissonlopes3@gmail.com>
2019-06-03 20:27:13 -0300

    feat: Inicialização do banco de professores

--- src/main/java/br/com/gtcc/util/InitDatabase.java
+++ src/main/java/br/com/gtcc/util/InitDatabase.java
@@ -7,13 +7,17 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 import br.com.gtcc.model.Aluno;
+import br.com.gtcc.model.Professor;
 import br.com.gtcc.repository.AlunoRepository;
+import br.com.gtcc.repository.ProfessorRepository;
 
 @Component
 public class InitDatabase implements ApplicationListener<ContextRefreshedEvent> {
 	
 	@Autowired
-	AlunoRepository repository;
+	ProfessorRepository repositoryProfessor;
+	@Autowired
+	AlunoRepository repositoryAluno;
 	
 	/*
 	 * Alunos inseridos de acordo com a planilha
@@ -76,19 +80,90 @@ public class InitDatabase implements ApplicationListener<ContextRefreshedEvent> 
 			aluno.setNomeUsuario("user"+dados[0]);
 			aluno.setTelefone("4002-8922");
 			
-			repository.save(aluno);
+			repositoryAluno.save(aluno);
 			
 		}
 	}
+	
+	
+	public void initTableProfessores()
+	{
+		String professores = "Thelma Elita Colanzi Lopes\r\n" + 
+				"Aline Maria Malachini Miotto Amaral\r\n" + 
+				"Raqueline Ritter de Moura Penteado\r\n" + 
+				"Edson Alves de Oliveira Junior\r\n" + 
+				"André Felipe Ribeiro Cordeiro\r\n" + 
+				"Lucas de Oliveira Teixeira\r\n" + 
+				"Airton Marco Polidório\r\n" + 
+				"Dante Alves Medeiros Filho\r\n" + 
+				"Nardênio Almeida Martins	\r\n" + 
+				"Rodrigo Calvo\r\n" + 
+				"Carlos Fransley Scatambulo Costa\r\n" + 
+				"Linnyer Beatrys Ruiz Aylon\r\n" + 
+				"Donizete Carlos Bruzarosco\r\n" + 
+				"André Noel\r\n" + 
+				"Wagner Igarashi\r\n" + 
+				"Flávio Arnaldo Braga da Silva\r\n" + 
+				"Marcos Aurélio Domingues	\r\n" + 
+				"Franklin César Flores\r\n" + 
+				"Valéria Delisandra Feltrim\r\n" + 
+				"Lucas Pupulin Nanni\r\n" + 
+				"Maurílio Martins Campano Junior\r\n" + 
+				"Daniel Kikuti\r\n" + 
+				"Ademir Aparecido Constantino\r\n" + 
+				"Yandre Maldonado e Gomes da Costa	\r\n" + 
+				"Josiane Melchiori Pinheiro\r\n" + 
+				"João Alencar Pamphile\r\n" + 
+				"Ronaldo Augusto de Lara Gonçalves\r\n" + 
+				"Paulo Roberto de Oliveira\r\n" + 
+				"André Barbosa Verona\r\n" + 
+				"Juliana Keiko Yamaguchi\r\n";
+		
+		insertProfessores(professores);
+		
+	}
+	
+	public void insertProfessores(String professores)
+	{	
+		String[] nomes = professores.split("\r\n");
+		int matricula = 10000;
+		for(String nome : nomes)
+		{
+			matricula++;
+			Professor professor = new Professor();
+			professor.setMatricula(matricula);
+			professor.setCoordenador(false);
+			professor.setNome(nome);
+			professor.setEmail(matricula+"@uem.br");
+			professor.setAtivo(1);
+			professor.setSenha(String.valueOf(matricula));
+			professor.setNomeUsuario("user"+matricula);
+			professor.setTelefone("4002-8922");
+			//seta a Thelma como coordenadora
+			if(nome.contains("Thelma Elita Colanzi"))
+			{
+				professor.setCoordenador(true);
+			}
+			
+			repositoryProfessor.save(professor);
+			
+		}
+	}
+	
 	
 	public void executar() {
 
 		try {
 			
-			if(repository.countRows() == 0)
+			if(repositoryAluno.countRows() == 0)
 			{
 				initTableAlunos();
 				System.out.println("Alunos cadastrados com sucesso!");
+			}
+			if(repositoryProfessor.countRows() == 0)
+			{
+				initTableProfessores();
+				System.out.println("Professores cadastrados com sucesso!");
 			}
 			
 		} catch (Exception e) {
