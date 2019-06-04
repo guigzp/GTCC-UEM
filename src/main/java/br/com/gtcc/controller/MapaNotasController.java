3edb3871a99ac7ea118b5ed8feab902690bf3648
Alan Lopes <alanlopes4@gmail.com>
2019-06-03 21:29:59 -0300

    fix: layout cadastro professor. feat: WIP mapa de notas

---
+++ src/main/java/br/com/gtcc/controller/MapaNotasController.java
@@ -0,0 +1,25 @@
+package br.com.gtcc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/gtcc/mapanotas")
+public class MapaNotasController {
+
+
+
+    @GetMapping
+    public ModelAndView findAll() {
+        ModelAndView mv = new ModelAndView("mapanotas/index");
+
+        return mv;
+    }
+
+
+    
+
+}
