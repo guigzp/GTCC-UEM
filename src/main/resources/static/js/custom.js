6a9706df3a1c9b53988920d2e993eca072c2841c
Alan Lopes <alanlopes4@gmail.com>
2019-06-04 21:19:09 -0300

    feat: Layou tela Cadastro de Aluno

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,31 +1,18 @@
 
-$(".form-group input").change(function() {
-  if ($(this).val() != "") {
-    $(this).addClass('filled');
-  } else {
-    $(this).removeClass('filled');
-  }
-})
-
-
 $(function () {
 	
-    $(".field-wrapper .field-placeholder").on("click", function () {
-        $(this).closest(".field-wrapper").find("input").focus();
-    });
-		
-		$(".field-wrapper input").blur(function(){
-			 var value = $.trim($(this).val());
-             if (value) {
-                 $(this).closest(".field-wrapper").addClass("hasValue");
-             } else {
-                 $(this).closest(".field-wrapper").removeClass("hasValue");
-             }
-		});
-			
-					
- 
-      
+	    $(".field-wrapper .field-placeholder").on("click", function () {
+	        $(this).closest(".field-wrapper").find("input").focus();
+	    });
+	    
+	    
+	    $('.field-wrapper input').each(function () {
+	    		var value = $.trim($(this).val());
+	             if (value) {
+	                 $(this).closest(".field-wrapper").addClass("hasValue");
+	             }
+	    	    });
+
             
             $(".field-wrapper input").on("keyup", function () {
                 var value = $.trim($(this).val());
