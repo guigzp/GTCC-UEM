9e6b7c52c8d858e15f1520efc8a7824fc015202e
guizamberlam <guizamberlam@hotmail.com>
2019-06-11 20:59:56 -0300

    Fix: Rota e tela proposta

--- src/main/java/br/com/gtcc/controller/PropostaController.java
+++ src/main/java/br/com/gtcc/controller/PropostaController.java
@@ -1,20 +1,31 @@
 package br.com.gtcc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.gtcc.model.Aluno;
+import br.com.gtcc.service.AlunoService;
+
 @Controller
 @RequestMapping("/gtcc/proposta")
 public class PropostaController {
 	
-	@GetMapping("/1")
-	public ModelAndView findAll(){
-		 ModelAndView mv = new ModelAndView("proposta/1");
-		 mv.addObject("proposta", true);
-	     return mv;
+	@Autowired
+	private AlunoService alunoService;
+	
+	@GetMapping
+	public ModelAndView index(){
+		List<Aluno> alunos = this.alunoService.buscarTodos();
+		ModelAndView mv = new ModelAndView("proposta/proposta");
+		mv.addObject("proposta", true);
+		mv.addObject("alunos", alunos);
+	    return mv;
 	}
 	
 	@PostMapping("/gerar")
