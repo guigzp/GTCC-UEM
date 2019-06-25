a810d6f91c86bb9b7418a04f2d35fc449c9c43b5
Alan Lopes <alan.lopes@eurekalabs.com.br>
2019-06-24 22:10:21 -0300

    fix: step input's notas

--- src/main/java/br/com/gtcc/controller/MapaNotasController.java
+++ src/main/java/br/com/gtcc/controller/MapaNotasController.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.http.MediaType;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import br.com.gtcc.model.Aluno;
 import br.com.gtcc.model.CriterioAvaliacao;
 import br.com.gtcc.model.FichaIdentificacao;
 import br.com.gtcc.model.MapaNotas;
